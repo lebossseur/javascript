57693c27b716a7a235c7b25b167f60f9483c1f1d
lebosseur <k_kjac@yahoo.fr>
2023-12-28 21:39:39 +0000

    scroll popup

---
+++ bubble/index.js
@@ -0,0 +1,16 @@
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+
+  bubble.style.setProperty("--left", Math.random() * 100 + "%");
+};
+
+setInterval(bubbleMaker, 300);
