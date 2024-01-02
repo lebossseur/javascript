da4f7bf225f1e75436b2f588d9d78e5ece0277fd
lebosseur <k_kjac@yahoo.fr>
2024-01-02 13:40:03 +0000

    debut sidebar

--- scroll/index.js
+++ scroll/index.js
@@ -8,8 +8,10 @@
 
 let lastScroll = 0;
 window.addEventListener("scroll", () => {
-  if (window.scrollY > lastScroll) {
-    navbar.style.height = "50px";
+  if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-60px";
   }
 
   lastScroll = window.scrollY;
